91823a6e59c668232c6b1f7f81a999c13922f996
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-12-07 18:01:08 +0200

    icloud-for-chrome v.1.0

---
+++ icloud-notes-for-chrome/src/js/background.js
@@ -0,0 +1,42 @@
+var idWindow;
+
+var urlWindow = 'https://www.icloud.com/#notes2';
+
+chrome.browserAction.onClicked.addListener(function() {
+
+	if (idWindow) {
+		chrome.windows.update(idWindow, {
+			focused : true
+		}, function(window) {
+			if (chrome.runtime.lastError) {
+				openWindow();
+			}
+		});
+	}
+
+	else {
+		openWindow();
+	}
+
+});
+
+function openWindow() {
+
+	var xWindow = screen.width - screen.availWidth + 25;
+	var yWindow = screen.height - screen.availHeight + 25;
+
+	var widthWindow = 1050;
+	var heightWindow = screen.height - yWindow - 25;
+
+	chrome.windows.create({
+		'url' : urlWindow,
+		'type' : 'popup',
+		'focused' : true,
+		'left' : xWindow,
+		'top' : yWindow,
+		'width' : widthWindow,
+		'height' : heightWindow
+	}, function(window) {
+		idWindow = window.id;
+	});
+}
