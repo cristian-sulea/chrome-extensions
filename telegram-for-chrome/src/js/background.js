367cae7cb631210cd4bc66133c228fc57791f90f
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-12-20 00:37:48 +0200

    telegram-for-chrome v.1.9 (remember window width)

--- telegram-for-chrome/src/js/background.js
+++ telegram-for-chrome/src/js/background.js
@@ -23,8 +23,14 @@ function openTelegramWindow() {
 	var telegramWindowX = screen.width - screen.availWidth + 25;
 	var telegramWindowY = screen.height - screen.availHeight + 25;
 
-	var telegramWindowWidth = Math.max(1050, Math.round(screen.availWidth / 2) - telegramWindowX - 25);
+	var telegramWindowWidth = 1050;
 	var telegramWindowHeight = screen.height - telegramWindowY - 25;
+
+	if (localStorage.telegramWindowWidth) {
+		telegramWindowWidth = Number(localStorage.telegramWindowWidth);
+	} else {
+		telegramWindowWidth = Math.max(telegramWindowWidth, Math.round(screen.availWidth / 2) - telegramWindowX - 25);
+	}
 
 	chrome.windows.create({
 		'url' : 'https://web.telegram.org/#/im',
@@ -37,4 +43,15 @@ function openTelegramWindow() {
 	}, function(window) {
 		idTelegramWindow = window.id;
 	});
+
 }
+
+setInterval(function() {
+	if (idTelegramWindow) {
+		chrome.windows.get(idTelegramWindow, function(window) {
+			if (!chrome.runtime.lastError) {
+				localStorage.telegramWindowWidth = window.width;
+			}
+		});
+	}
+}, 5000);
